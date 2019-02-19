76c6d187aba0e7f833192a844d6adce244b89fc1
Kayla Fang <kfang00@stuy.edu>
2019-02-18 22:07:08 -0500

    started countSolutions

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,7 +1,8 @@
 public class KnightBoard {
   private int[][]board;
+  private int count;
 
-  @throws IllegalArgumentException when either parameter is negative.
+  //@throws IllegalArgumentException when either parameter is negative;
   public KnightBoard(int startingRows,int startingCols) {
     if ((startingRows <= 0) || (startingCols <= 0)) {
       throw new IllegalArgumentException();
@@ -25,7 +26,7 @@ public class KnightBoard {
         if (board[a][b] == 0) {
           s += "  _";
         }
-        if (board.length * board[0].length < 10) {
+        else if (board.length * board[0].length < 10) {
           s += "  " + board[a][b];
         }
         else {
@@ -57,15 +58,18 @@ public class KnightBoard {
     if (!checkZero()) {
       throw new IllegalStateException();
     }
-    if ((startingRows < 0) || (startingCols < 0) || (startingRows >= board.length) || (startingCols >= board[0].length)) {
+    if ((startingRow < 0) || (startingCol < 0) || (startingRow >= board.length) || (startingCol >= board[0].length)) {
       throw new IllegalArgumentException();
     }
     return solveH(startingRow, startingCol, 1);
   }
 
   public boolean solveH(int staRow, int staCol, int level) {
-    if (curNum == ((staRow * staCol) + 1)) {
+    if (level == ((board.length * board[0].length) + 1)) {
       return true;
+    }
+    if ((staRow >= board.length) || (staCol >= board[0].length) || (staRow < 0) || (staCol < 0)) {
+      return false;
     }
     if (placeKnight(staRow, staCol, level)) {
       if (solveH(staRow + 2, staCol + 1, level + 1)) {
@@ -113,9 +117,64 @@ public class KnightBoard {
     return false;
   }
 
+  public int countSolutions(int startingRow, int startingCol) {
+    if (!checkZero()) {
+      throw new IllegalStateException();
+    }
+    if ((startingRow < 0) || (startingCol < 0) || (startingRow >= board.length) || (startingCol >= board[0].length)) {
+      throw new IllegalArgumentException();
+    }
+    return countH(startingRow, startingCol);
+  }
+
+  public int countH(int staRow, int staCol) {
+    if (level == ((board.length * board[0].length) + 1)) {
+      return true;
+    }
+    if ((staRow >= board.length) || (staCol >= board[0].length) || (staRow < 0) || (staCol < 0)) {
+      return false;
+    }
+    if (placeKnight(staRow, staCol, level)) {
+      if (solveH(staRow + 2, staCol + 1, level + 1)) {
+	return true;
+      }
+      if (solveH(staRow + 2, staCol - 1, level + 1)) {
+	return true;
+      }
+      if (solveH(staRow + 1, staCol + 2, level + 1)) {
+	return true;
+      }
+      if (solveH(staRow - 1, staCol + 2, level + 1)) {
+	return true;
+      }
+      if (solveH(staRow - 2, staCol + 1, level + 1)) {
+	return true;
+      }
+      if (solveH(staRow - 2, staCol - 1, level + 1)) {
+	return true;
+      }
+      if (solveH(staRow + 1, staCol - 2, level + 1)) {
+	return true;
+      }
+      if (solveH(staRow - 1, staCol - 2, level + 1)) {
+	return true;
+      }
+      removeKnight(staRow, staCol);
+    }
+    return false;
+  }
+
   public static void main(String[] args) {
     KnightBoard k = new KnightBoard(5, 5);
+    System.out.println(k);
     System.out.println(k.solve(0, 0));
+    System.out.println(k);
+    KnightBoard a = new KnightBoard(6, 6);
+    System.out.println(a.solve(0, 0));
+    System.out.println(a);
+    KnightBoard ab = new KnightBoard(6, 7);
+    System.out.println(ab.solve(0, 0));
+    System.out.println(ab);
   }
 
 
