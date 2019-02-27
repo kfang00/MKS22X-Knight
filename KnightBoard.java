858dde8b79c835e1f1e7b7875af9d32ab82c1ddf
Kayla Fang <kfang00@stuy.edu>
2019-02-26 21:42:00 -0500

    Code for outgoingMoves()

--- KnightBoard.java
+++ KnightBoard.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 
 public class KnightBoard {
   private int[][]board;
+  private int[][]moves;
   private int count;
   private ArrayList<Arrays> coor;
 
@@ -12,15 +13,37 @@ public class KnightBoard {
       throw new IllegalArgumentException();
     }
     board = new int[startingRows][startingCols];
+    moves = new int[startingRows][startingCols];
     coor = new ArrayList<Arrays>(8);
-    removeNull();
+    removeNull(board);
+    removeNull(moves);
+    outgoingMoves();
   }
 
   public void outgoingMoves() {
-    String s = "";
+    moves[0][0] = 2; moves[0][moves[0].length - 1] = 2; moves[moves.length - 1][0] = 2; moves[moves.length - 1][moves[0].length - 1] = 2;
+    moves[0][1] = 3; moves[0][moves[0].length - 2] = 3; moves[moves.length - 1][1] = 3; moves[moves.length - 1][moves[0].length - 2] = 3;
+    moves[1][0] = 3; moves[1][moves[0].length - 1] = 3; moves[moves.length - 2][0] = 3; moves[moves.length - 2][moves[0].length - 1] = 3;
+    moves[1][1] = 4; moves[1][moves[0].length - 2] = 4; moves[moves.length - 2][1] = 4; moves[moves.length - 2][moves[0].length - 2] = 4;
+    for (int a = 0; a < moves.length; a++) {
+      for (int b = 0; b < moves[0].length; b++) { 
+        if (moves[a][b] == 0) {
+	  if ((a == 0) || (a == (moves.length - 1)) || (b == 0) || (b == (moves[0].length - 1))) {
+	    moves[a][b] = 4;
+	  }
+	  else if ((a == 1) || (a == (moves.length - 2)) || (b == 1) || (b == (moves[0].length - 2))) {
+	    moves[a][b] = 6;
+	  }
+	  else {
+	    moves[a][b] = 8;
+	  }
+   	}
+      }
+    }
 
   }
-  public void removeNull() {
+
+  public void removeNull(int[][] board) {
     for (int a = 0; a < board.length; a++) {
       for (int b = 0; b < board[0].length; b++) {
 	       board[a][b] = 0;
@@ -52,6 +75,17 @@ public class KnightBoard {
     return s;
   }
 
+  public String toStringC(){
+    String s = "";
+    for (int a = 0; a < moves.length; a++) {
+      for (int b = 0; b < moves[0].length; b++) {
+        s += "  " + moves[a][b];
+      }
+      s += "\n";
+    }
+    return s;
+  }
+
   private boolean checkZero() {
     for (int a = 0; a < board.length; a++) {
       for (int b = 0; b < board[0].length; b++) {
@@ -63,9 +97,9 @@ public class KnightBoard {
     return true;
   }
 
-  pubilc boolean findCoor() {
+  //pubilc void findCoor() {
 
-  }
+//  }
 
   public boolean solve(int startingRow, int startingCol) {
     if (!checkZero()) {
@@ -172,15 +206,15 @@ public class KnightBoard {
 
   public static void main(String[] args) {
     KnightBoard k = new KnightBoard(5, 5);
-    System.out.println(k);
+    System.out.println(k.toStringC());
     System.out.println(k.solve(0, 0));
     System.out.println(k);
     KnightBoard a = new KnightBoard(6, 6);
     System.out.println(a.solve(0, 0));
     System.out.println(a);
-    KnightBoard ab = new KnightBoard(6, 6);
-    System.out.println(ab.countSolutions(0, 0));
-    System.out.println(ab);
+    //KnightBoard ab = new KnightBoard(6, 6);
+    //System.out.println(ab.countSolutions(0, 0));
+    //System.out.println(ab);
   }
 
 
